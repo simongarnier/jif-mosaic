cfd32d4641375212a4eaa30c9b4f5c8d1dbbf4ab
Simon Garnier <simongarnierg@gmail.com>
2016-03-12 22:22:45 -0500

    you can now click on jifs to show them

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :jifs, only: [:index, :new, :create]
+  resources :jifs, only: [:index, :new, :create, :show]
   root 'jifs#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
